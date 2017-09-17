d48eac225e5e6910b684b7b7658be044f3f956fd
Yosef Asouaqir <yosef.alsuoaqir@gmail.com>
2017-09-17 14:53:07 +0300

    updated

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -54,9 +54,128 @@ $("#footerContacts").append(HTMLlocation.replace("%data%", bio.contacts.location
 
 bio.display();
 
+var work = {
+
+  jobs: [{
+    employer: "google",
+    title: "Web developer",
+    location: "Riyadh, SA",
+    dates: "20/10/2017 - Present",
+    description: "do a web developer"
+  }, {
+    employer: "google",
+    title: "Web developer",
+    location: "Riyadh, SA",
+    dates: "20/10/2017 - Present",
+    description: "do a web developer"
+  }],
+
+  display: function() {
+
+
+for (var i= 0; i< work.jobs.length; i++)  {
+  $("#workExperience").append(HTMLworkStart.replace("%data%", work.jobs));
+
+    $("#workExperience").append(HTMLworkEmployer.replace("%data%", work.jobs[i].employer));
+    $("#workExperience").append(HTMLworkTitle.replace("%data%", work.jobs[i].title));
+    $("#workExperience").append(HTMLworkDates.replace("%data%", work.jobs[i].dates));
+    $("#workExperience").append(HTMLworkLocation.replace("%data%", work.jobs[i].location));
+    $("#workExperience").append(HTMLworkDescription.replace("%data%", work.jobs[i].description));
+}
+
+
+  }
+
+};
+work.display();
+
+var projects= {
+
+  projects: [{ /*array of objects with*/
+    title: "Strava",
+    dates: "10/10/2017",
+    /*string (works with a hyphen between them)*/
+    description: "sport app",
+    images: ["https://s5.mzstatic.com/eu/r30/Purple128/v4/1d/00/ef/1d00efcb-cb7e-64bf-c0ab-fc91ccbbbbcb/icon190x190.jpeg",
+
+"https://s5.mzstatic.com/eu/r30/Purple128/v4/1d/00/ef/1d00efcb-cb7e-64bf-c0ab-fc91ccbbbbcb/icon190x190.jpeg"]
+  }],
+  display: function() {
+
+    $("#projects").append(HTMLprojectStart.replace("%data%", projects));
+for (var i= 0; i<projects.projects.length; i++){
+    $("#projects").append(HTMLprojectTitle.replace("%data%", projects.projects[i].title));
+    $("#projects").append(HTMLprojectDates.replace("%data%", projects.projects[i].dates));
+    $("#projects").append(HTMLprojectDescription.replace("%data%", projects.projects[i].description));
+    for (var j= 0; j< projects.projects[i].images.length; j++)
+    $("#projects").append(HTMLprojectImage.replace("%data%", projects.projects[i].images[j]));
+
+  }
+}
+};
+projects.display();
+
+var education = {
+
+  schools: [{
+    name: "King Saud Uneversity",
+    location: "Riyadh",
+    degree: "Bacholer",
+    majors: ["IS", "MIS", "CS"],
+    /*array of Sring*/
+    dates: "20/1/2017",
+    /*string (works with a hyphen between them)*/
+    url: "www.schools.com",
+    /*(optional)*/
+  }, {
+    name: "King Saud Uneversity",
+    location: "Riyadh",
+    degree: "Bacholer",
+    majors: ["IS", "MIS", "CS"],
+    /*array of Sring*/
+    dates: "20/1/2017",
+    /*string (works with a hyphen between them)*/
+    url: "www.schools.com",
+    /*(optional)*/
+  }],
+  onlineCourses: [{ /*array of objects with*/
+    title: "Cloud Computing",
+    school: "Dell",
+    dates: "20/1/2017",
+    /*string (works with a hyphen between them)*/
+    url: "www.onlineCourses.com"
+  }, { /*array of objects with*/
+    title: "Cloud Computing",
+    school: "Dell",
+    dates: "20/1/2017",
+    /*string (works with a hyphen between them)*/
+    url: "www.onlineCourses.com"
+  }],
+
+  display: function() {
+
+    $("#education").append(HTMLschoolStart.replace("%data%", education));
+for (var i= 0; i<education.schools.length; i++){
+    $("#education").append(HTMLschoolName.replace("%data%", education.schools[i].name));
+    $("#education").append(HTMLschoolDegree.replace("%data%", education.schools[i].degree));
+    $("#education").append(HTMLschoolDates.replace("%data%", education.schools[i].dates));
+    $("#education").append(HTMLschoolLocation.replace("%data%", education.schools[i].location));
+    for (var j= 0; j<education.schools[i].majors.length; j++){
+    $("#education").append(HTMLschoolMajor.replace("%data%", education.schools[i].majors[j]));
+}}
+for (var h= 0; h<education.onlineCourses.length; h++){
+
+    $("#education").append(HTMLonlineClasses.replace("%data%", education.onlineCourses));
+    $("#education").append(HTMLonlineTitle.replace("%data%", education.onlineCourses[h].title));
+    $("#education").append(HTMLonlineSchool.replace("%data%", education.onlineCourses[h].school));
+    $("#education").append(HTMLonlineDates.replace("%data%", education.onlineCourses[h].dates));
+    $("#education").append(HTMLonlineURL.replace("%data%", education.onlineCourses[h].url));
+}
+
+
+  }
+};
+education.display();
 
 
 $("#mapDiv").append(googleMap);
-// This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is'==/an_example/){of_beautifier();}else{var a=b?(c%d):e[f];}
